68a2b9927cc7331dff1955d90dead4f718406fc6
legends-killer <958775729@qq.com>
2020-03-01 14:58:29 +0800

    adjuest some params

--- test.py
+++ test.py
@@ -3,10 +3,11 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier('./temp/cascade.xml')
 
-img = cv2.imread('./images/t.jpg')
+img = cv2.imread('./images/t2.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+faces = face_cascade.detectMultiScale(gray, 10, 5, cv2.CASCADE_SCALE_IMAGE, (150,150), (500,500))
 
 for (x, y, w, h) in faces:
     cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
