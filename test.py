eb80339ae969edae64f34e19dca9d76820651a15
legends-killer <958775729@qq.com>
2020-03-01 17:45:59 +0800

    add more test samples and fix some params

--- test.py
+++ test.py
@@ -3,15 +3,16 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier('./temp/cascade.xml')
 
-img = cv2.imread('./images/t2.jpg')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+for i in range(1,7):
+    name = './images/t'+str(i)+'.jpg'
+    img = cv2.imread(name)
+    img = cv2.resize(img, (600, 450))
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 10, 5, cv2.CASCADE_SCALE_IMAGE, (150,150), (600,600))
 
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-faces = face_cascade.detectMultiScale(gray, 10, 5, cv2.CASCADE_SCALE_IMAGE, (150,150), (500,500))
-
-for (x, y, w, h) in faces:
-    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
-cv2.imshow('img', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    cv2.imshow('img', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
