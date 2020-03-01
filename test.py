bf7b9967ad7c300ffba5b51e1d3dd26d75aad09e
legends-killer <958775729@qq.com>
2020-03-01 17:57:11 +0800

    fix some params

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
 import numpy as np
 import cv2
 
-face_cascade = cv2.CascadeClassifier('./temp/cascade.xml')
+cascade = cv2.CascadeClassifier('./temp/cascade.xml')
 
 for i in range(1,7):
     name = './images/t'+str(i)+'.jpg'
     img = cv2.imread(name)
     img = cv2.resize(img, (600, 450))
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 10, 5, cv2.CASCADE_SCALE_IMAGE, (150,150), (600,600))
+    doors = cascade.detectMultiScale(gray, 10, 5, cv2.CASCADE_SCALE_IMAGE, (150,150), (600,600))
 
-    for (x, y, w, h) in faces:
+    for (x, y, w, h) in doors:
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
     cv2.imshow('img', img)
